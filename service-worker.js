0982d7f6b087337398b15cd2732139e5dd710708
Sibrand Hoekstra <sibrand@gmail.com>
2019-10-06 22:52:19 +0200

    PWA v0.1

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+      caches.open('nextweek').then(function(cache) {
+        return cache.addAll([
+          './',
+          './index.html',
+          './index.html?homescreen=1',
+          './?homescreen=1',
+          './style.css',
+          './main.js',
+          './icon.png',
+          './icon-512.png'
+        ]);
+      })
+    );
+   });
+
+self.addEventListener('fetch', function(event) {
+
+    console.log(event.request.url);    
+
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    
+    );
+    
+});
